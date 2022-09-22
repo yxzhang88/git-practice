f99728025693b6abfc153a027396621346fd2b02
wanjunlan <wanjunlan@gmail.com>
2022-09-22 13:51:35 -0700

    completed function_a

--- function_a.py
+++ function_a.py
@@ -1,7 +1,21 @@
 def most_common_value(number_list):
     """ returns the most common element of the list
     """
-    pass
+    frequency_index = {}
+    max_frequency = -1
+    most_common_value = None
+    for num in number_list:
+        if frequency_index.get(num):
+            frequency_index[num] += 1
+        else:
+            frequency_index[num] = 1
+
+        if max_frequency < frequency_index[num]:
+            max_frequency = frequency_index[num]
+            most_common_value = num
+
+    return most_common_value
+    
 
 
 if __name__ == "__main__":
